f139dbef65edfd901c9bc7433ee96c3012c27527
omkarkale2810 <omkarkale7888@gmail.com>
2023-08-23 15:58:38 +0530

    timer added

--- script2.js
+++ script2.js
@@ -1,17 +1,36 @@
 const questionElement = document.getElementById("question-text");
 const options = document.querySelectorAll(".option");
+const timer = document.getElementById("timer");
 
+let c = 0;
+let w = 0;
+let tl = 0;
 const questions = [
   {
     question: "What is the capital of France?",
     choices: ["Paris", "Madrid", "Rome", "Berlin"],
-    correctAnswer: 0
+    correctAnswer: 0,
   },
   {
     question: "Which planet is known as the Red Planet?",
     choices: ["Earth", "Mars", "Venus", "Jupiter"],
-    correctAnswer: 1
-  }
+    correctAnswer: 1,
+  },
+  {
+    question: "Which of the given is a disease caused by protozoa?",
+    choices: ["Cancer", "Typhoid", "Kala-azar", "Chicken Pox"],
+    correctAnswer: 2,
+  },
+  {
+    question: "The tropic of cancer does pass through which state of India?",
+    choices: ["Uttar Pradesh", "Madhya Pradesh", "Bihar", "Andhra Pradesh"],
+    correctAnswer: 1,
+  },
+  {
+    question: "Which of the following is a scalar quantity??",
+    choices: ["Force", "Pressure", "Momentum", "Acceleration"],
+    correctAnswer: 1,
+  },
 ];
 
 let currentQuestionIndex = 0;
@@ -19,7 +38,7 @@ let currentQuestionIndex = 0;
 function showQuestion() {
   const currentQuestion = questions[currentQuestionIndex];
   questionElement.textContent = currentQuestion.question;
-  
+
   options.forEach((option, index) => {
     option.textContent = currentQuestion.choices[index];
   });
@@ -27,22 +46,43 @@ function showQuestion() {
 
 function checkAnswer(selectedIndex) {
   const currentQuestion = questions[currentQuestionIndex];
-  
+
   if (selectedIndex === currentQuestion.correctAnswer) {
-    alert("Correct answer!");
+    // alert("Correct answer!");
+    c++;
   } else {
-    alert("Wrong answer. Try again.");
+    // alert("Wrong answer. Try again.");
+    w++;
   }
-  
-  // Move to the next question or end the quiz
+
   currentQuestionIndex++;
   if (currentQuestionIndex < questions.length) {
     showQuestion();
   } else {
     alert("Quiz finished!");
-    // You can perform additional actions here after the quiz ends.
+    na = questions.length - c - w;
+    window.location = `/result.html?c=${c}&w=${w}&na=${na}&tl=${tl}`;
   }
 }
 
-// Start the quiz
+function showTimer() {
+  counter = 50;
+  let id = setInterval(() => {
+    if (counter > 0) {
+      timer.innerText = "Time Left: " + counter;
+      counter--;
+      tl = counter;
+    } else {
+      clearInterval(id);
+      timer.innerText = "Time Over!";
+      window.alert("Submit Quiz");
+      tl = counter;
+      na = questions.length - c - w;
+      window.location = `/result.html?c=${c}&w=${w}&na=${na}&tl=${tl}`;
+    }
+  }, 1000);
+}
+
+showTimer();
 showQuestion();
+console.log(window.location);
